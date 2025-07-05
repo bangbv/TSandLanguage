72b68a2e700c6a332746ea34a7288f1860def3d5
bang.bui <bang.bui@mservice.com.vn>
2025-07-05 13:49:29 +0700

    add main llama

---
+++ text_aid_forecast/tsllm/main_llama.py
@@ -0,0 +1,46 @@
+import hydra
+from omegaconf import DictConfig
+import os , sys
+from tsllm.models.utils_llama import load_model_by_name , get_predict_results
+from tsllm.datasets_2 import get_datasets
+from tsllm.pre_processing_2 import pre_processing
+import os , pickle , time
+
+from tsllm.token_utils import build_save_path  , is_completion
+from tsllm.models.utils_llama import print_debug, my_print
+
+
+@hydra.main(config_path="config", config_name="config", version_base="1.2")
+def run(config: DictConfig):
+    debug_mode = config.debug_mode
+    is_test_mode = config.is_test_mode
+    print_debug(my_print, "Running with config", config, debug_mode)
+    datasets = get_datasets(config)
+    print_debug(my_print, "Length of datasets:", len(datasets), debug_mode)
+    model = load_model_by_name(config)
+
+    num_samples = 20 if 'gpt' in config.model.name else 96
+    batch_size =  0  if 'gpt' in config.model.name else 6
+
+    scalers = None
+    save_dir = build_save_path(config)
+    print_debug(my_print, "Save dir:", len(datasets), debug_mode)
+    for dsname,data in datasets.items():
+        if is_completion(save_dir , dsname, is_test_mode) : continue
+        outs_dict = {}
+        train, test , description= data
+        # print(train , ' -- tes tLen:' , len(test) )
+        _, input_strs ,  scalers , test  = pre_processing(train, test , description , config , model.tokenizer, debug_mode )
+        # print(input_strs)
+        try:
+            out = get_predict_results(model , input_strs  , test , description  , config, batch_size, num_samples, scalers = scalers )
+            print(f"main:run: the result {out}")
+            outs_dict[config.model.name] = out
+        except Exception as e:
+            print(f"Failed {dsname} {config.model.name}" + str(e) )
+            continue
+        with open(f'{save_dir}/{dsname}.pkl','wb') as f:
+            pickle.dump(outs_dict,f)
+
+if __name__ == "__main__":
+    run()
--- text_aid_forecast/tsllm/models/utils_llama.py
+++ text_aid_forecast/tsllm/models/utils_llama.py
@@ -1,4 +1,3 @@
-from tsllm.models.gpt_2 import GPTmodel
 from tsllm.models.llama_2 import LLAMAmodel
 from tqdm import tqdm
 
@@ -16,8 +15,6 @@ import tsllm.models.fourier_transforms as ft
 STEP_MULTIPLIER = 1.2 
     
 def load_model_by_name(config):
-    if config.model.name in ['gpt-3.5-turbo','gpt-4']:
-        return GPTmodel(config=config)
     if config.model.name in ['llama-7b', 'llama-13b', 'llama-70b', 'llama-7b-chat', 'llama-13b-chat', 'llama-70b-chat']:
         return LLAMAmodel(config=config)
     return None
