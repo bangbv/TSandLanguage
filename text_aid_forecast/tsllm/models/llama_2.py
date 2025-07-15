08125af72c9ab4f33764942a904aac841156456a
bang.bui <bang.bui@mservice.com.vn>
2025-07-15 17:56:27 +0700

    fix config

--- text_aid_forecast/tsllm/models/llama_2.py
+++ text_aid_forecast/tsllm/models/llama_2.py
@@ -81,15 +81,17 @@ class LLAMAmodel(torch.nn.Module):
         tokenizer = self.get_tokenizer(model_name)
         return tokenizer(str)
 
-    def run(self, input_str, description, steps, settings, batch_size, num_samples, temp):
+    def run(self, input_str, description, steps, config, batch_size, num_samples, temp):
         print_debug(my_print, "LLAMAmodel:run:input_str:", input_str,True)
         print_debug(my_print, "LLAMAmodel:run:description:", description,True)
         print_debug(my_print, "LLAMAmodel:run:steps:", steps,True)
         print_debug(my_print, "LLAMAmodel:run:batch_size:", batch_size,True)
         print_debug(my_print, "LLAMAmodel:run:num_samples:", num_samples,True)
         print_debug(my_print, "LLAMAmodel:run:temp:", temp,True)
-        model_name = settings.model.model_name
+        model_name = config.model.model_name
+        settings = config.model.settings
         print_debug(my_print, "LLAMAmodel:run:model_name:", model_name, True)
+        print_debug(my_print, "LLAMAmodel:run:settings:", settings, True)
         if self.task == 'forecast':
             return self.forecast(model_name, input_str, steps, description, settings, num_samples, temp)
 
@@ -103,8 +105,8 @@ class LLAMAmodel(torch.nn.Module):
         print_debug(my_print, "LLAMAmodel:forecast:temp:", temp,True)
         print_debug(my_print, "LLAMAmodel:forecast:top_p:", top_p,True)
         print_debug(my_print, "LLAMAmodel:forecast:cache_model:", cache_model,True)
-        print_debug(my_print, "LLAMAmodel:forecast:settings.model.time_sep:", settings.model.time_sep,True)
-        avg_tokens_per_step = len(self.tokenize_fn(input_str, model_name)['input_ids']) / len(input_str.split(settings.model.time_sep))
+        print_debug(my_print, "LLAMAmodel:forecast:settings.time_sep:", settings.time_sep,True)
+        avg_tokens_per_step = len(self.tokenize_fn(input_str, model_name)['input_ids']) / len(input_str.split(settings.time_sep))
         max_tokens = int(avg_tokens_per_step * steps)
 
         model, tokenizer = self.get_model_and_tokenizer(model_name, cache_model=cache_model)
@@ -120,7 +122,7 @@ class LLAMAmodel(torch.nn.Module):
             batch = {k: v.cuda() for k, v in batch.items()}
             num_input_ids = batch['input_ids'].shape[1]
 
-            good_tokens_str = list("0123456789" + settings.model.time_sep)
+            good_tokens_str = list("0123456789" + settings.time_sep)
             good_tokens = [tokenizer.convert_tokens_to_ids(token) for token in good_tokens_str]
             # good_tokens += [tokenizer.eos_token_id]
             bad_tokens = [i for i in range(len(tokenizer)) if i not in good_tokens]
