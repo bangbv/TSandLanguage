3a3fbec0612ea9dd7f7218a2b13ec30b1efe2a8e
bang.bui <bang.bui@mservice.com.vn>
2025-09-05 13:38:36 +0700

    update parameter for run function

--- text_aid_forecast/tsllm/models/llama_new.py
+++ text_aid_forecast/tsllm/models/llama_new.py
@@ -75,11 +75,11 @@ class LLAMAmodel(torch.nn.Module):
         tokenizer = self.load_tokenizer(model_name)
         return tokenizer(str)
 
-    def run(self, input_str, description, steps, config, batch_size, num_samples, temp):
+    def run(self, input_str, input_trend_str, input_season_str, input_resid_str, description, steps, config, batch_size, num_samples, temp):
         model_name = config.model.model_name
         settings = config.model.settings
         if self.task == 'forecast':
-            return self.forecast(model_name, input_str, steps, settings, batch_size, num_samples, temp)
+            return self.forecast(model_name, input_str, input_trend_str, input_season_str, input_resid_str, steps, settings, batch_size, num_samples, temp)
 
     def forecast(self, model_name, input_str, input_trend_str, input_season_str, input_resid_str, steps, settings, batch_size=5, num_samples=20, temp=0.9, top_p=0.9, cache_model=True):
         # debug_mode = self.debug_mode
